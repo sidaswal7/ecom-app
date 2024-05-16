45544b80fffd05e2757fb95cf313b55257b2c6a6
sidaswal7 <siddhantaswal7@gmail.com>
2024-05-16 11:46:17 +0530

    created folder structure

---
+++ src/assets/dummyData.js
@@ -0,0 +1,27 @@
+
+export const dummyData = [
+    {
+      id: 'p1',
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    },
+    {
+      id: 'p2',
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    },
+    {
+      id: 'p3',
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    },
+    {
+      id: 'p4',
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    },
+  ];
---
+++ src/components/Header/CartButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const CartButton = ()=> {
+    return(
+        <>
+
+        </>
+    )
+}
+
+export default CartButton;
